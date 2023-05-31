557d28e7e68cf77906cb17f2a4055479b801b571
Felipe <juanftorrest@gmail.com>
2023-05-30 23:33:35 -0500

    Version 1 calculadora

---
+++ script.js
@@ -0,0 +1,45 @@
+let display = document.getElementById('display');
+
+let numeros = document.querySelectorAll("button");
+
+//console.log(typeof display.innerHTML);
+
+
+for (const numero of numeros) {
+    
+    numero.addEventListener("click", () =>{
+
+        
+        if ( numero.id === "ac" ) {
+            display.innerHTML = "";
+        }else if ( numero.id === "btn-equal" && display.innerHTML !== "" ) {
+            display.innerHTML = eval( display.innerHTML )
+        }else if ( numero.id === "erase" && display.innerHTML !== "" ) {
+            let string = display.innerHTML.toString();
+            
+            display.innerHTML = string.substring(0, string.length-1);
+        }
+        else{
+
+            display.innerHTML += numero.id;
+            display.scrollTop = display.scrollHeight;
+            display.readOnly = true;
+            
+        }
+        
+
+        
+    })
+}
+
+const botonLight = document.querySelector("#dark");
+const calculator = document.querySelector(".calculadora-wrapper")
+const iconlight = document.querySelector("ion-icon")
+
+let islight = true;
+
+botonLight.onclick=()=>{
+    calculator.classList.toggle("light");
+    botonLight.classList.toggle("dark");
+    islight = !islight;
+}
